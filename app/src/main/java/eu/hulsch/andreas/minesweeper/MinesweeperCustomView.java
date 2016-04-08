5b7dbc18c6537f2d72473064c18a4d86913872bb
murphy1312 <murphy@woweed.de>
2016-04-08 17:21:38 +0100

    Milestone 3: black background and white seperator lines

--- app/src/main/java/eu/hulsch/andreas/minesweeper/MinesweeperCustomView.java
+++ app/src/main/java/eu/hulsch/andreas/minesweeper/MinesweeperCustomView.java
@@ -2,6 +2,9 @@ package eu.hulsch.andreas.minesweeper;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,7 +15,13 @@ import android.view.View;
 public class MinesweeperCustomView extends View
 {
     private int width_height;
-    private int cell_width_height;
+    private double cell_width_height;
+
+    private Paint paint_black;
+    private Paint paint_white;
+
+    private float[] pointsVertical;
+    private float[] pointsHorizontal;
 
     // ctor
     public MinesweeperCustomView(Context context)
@@ -32,13 +41,25 @@ public class MinesweeperCustomView extends View
 
     private void init()
     {
+        paint_black = new Paint();
+        paint_black.setColor(Color.BLACK);
+
+        paint_white = new Paint();
+        paint_white.setColor(Color.WHITE);
+        paint_white.setStyle(Paint.Style.STROKE);
 
     }
 
-    // gets called often
+    // gets called very often
     public void onDraw(Canvas canvas)
     {
-        super.onDraw(canvas);
+        //super.onDraw(canvas);
+        canvas.drawRect(0, 0, this.width_height, this.width_height, this.paint_black);
+        // canvas.drawRect(0,0, this.width_height, this.width_height, this.paint_white);
+
+        canvas.drawLines(this.pointsHorizontal, this.paint_white);
+        canvas.drawLines(this.pointsVertical, this.paint_white);
+
 
     }
 
@@ -51,10 +72,14 @@ public class MinesweeperCustomView extends View
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
     {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        setPadding(0,0,0,0);
+
         int height = getMeasuredHeight();
         int width = getMeasuredWidth();
 
-        if (height >= width)
+        if (height > width)
         {
             this.width_height = width;
         }
@@ -62,8 +87,31 @@ public class MinesweeperCustomView extends View
         {
             this.width_height = height;
         }
+        this.cell_width_height = (double) this.width_height/10;
+
+        this.pointsVertical = new float[44];
+        for(int i = 0; i < this.pointsVertical.length; i=i+4)
+        {
+            // first point x, y
+            this.pointsVertical[i] = (float) (this.cell_width_height * (i/4));
+            this.pointsVertical[i+1] = 0f;
+            // second point x, y
+            this.pointsVertical[i+2] = (float) (this.cell_width_height*(i/4));
+            this.pointsVertical[i+3] = (float) this.width_height;
+        }
+
+        this.pointsHorizontal = new float[44];
+        for(int i = 0; i < this.pointsHorizontal.length; i=i+4)
+        {
+            // first point x, y
+            this.pointsHorizontal[i] = 0f;
+            this.pointsHorizontal[i+1] = (float) (this.cell_width_height * (i/4));
+            // second point x, y
+            this.pointsHorizontal[i+2] = (float) this.width_height;
+            this.pointsHorizontal[i+3] = (float) (this.cell_width_height*(i/4));
+        }
+
         setMeasuredDimension(this.width_height, this.width_height);
-
-
     }
+
 }
